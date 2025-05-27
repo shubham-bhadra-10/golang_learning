2ca53ffbd2cd6be12382538b46f9a3b36f0dd75e
shubham-bhadra-10 <shubhambhadra10@gmail.com>
2025-05-27 09:13:13 -0400

    Setting up router and cors

--- myProject/go.mod
+++ myProject/go.mod
@@ -2,4 +2,8 @@ module github.com/shubham-bhadra-10/golang_learning/myProject
 
 go 1.24.3
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/cors v1.2.1
+	github.com/joho/godotenv v1.5.1
+)
--- myProject/main.go
+++ myProject/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -16,5 +19,34 @@ func main() {
 		// log.Fatal is used to stop the program and print an error message
 		log.Fatal("Port is not found in environment")
 	}
+	router := chi.NewRouter()
+	// chi is a web framework that is used to create a router
+	// chi.NewRouter() is used to create a new router
+
+	// cors is a middleware that is used to allow cross-origin requests
+	router.Use(cors.Handler(
+		cors.Options{
+			AllowedOrigins: []string{"https://*", "http://*"},
+			AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders: []string{"*"},
+			ExposedHeaders: []string{"link"},
+			AllowCredentials: false,
+			MaxAge: 300,
+		},
+	))
+
+	// srv is a server that is used to start the server
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":" + portString,
+	}
+	fmt.Printf("Starting server on port %v\n", portString)
+	// srv.ListenAndServe() is used to start the server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	
 	fmt.Println("Port: ", portString)
 }
